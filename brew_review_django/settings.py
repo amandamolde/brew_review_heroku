3585c4214007d81665d1cd5fa5b9a3290ffa89cf
amandamolde <amanda.molde@gmail.com>
2018-10-29 09:26:37 -0600

    Added static root and static files storage

--- brew_review_django/settings.py
+++ brew_review_django/settings.py
@@ -17,6 +17,8 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
