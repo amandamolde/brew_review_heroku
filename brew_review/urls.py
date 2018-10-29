637f2179e4eabedefa2de7b33f197ce1feb96419
amandamolde <amanda.molde@gmail.com>
2018-10-29 10:59:33 -0600

    Changed back position of api/

--- brew_review/urls.py
+++ brew_review/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('breweries/', views.BreweryList.as_view(), name='brewery-list'),
-    path('breweries/<int:pk>', views.BreweryDetail.as_view(), name='brewery-detail'),
-    path('reviews/', views.ReviewList.as_view(), name='review-list'),
-    path('reviews/<int:pk>', views.ReviewDetail.as_view(), name='review-detail')
+    path('api/breweries/', views.BreweryList.as_view(), name='brewery-list'),
+    path('api/breweries/<int:pk>', views.BreweryDetail.as_view(), name='brewery-detail'),
+    path('api/reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('api/reviews/<int:pk>', views.ReviewDetail.as_view(), name='review-detail')
 ]
--- brew_review_django/urls.py
+++ brew_review_django/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('brew_review.urls')),
+    path('', include('brew_review.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
